44f77ddb9f1edac2300a180043b3270a3245d10a
Benedek Kozma <benedek.kozma@tresorit.com>
2019-11-27 19:08:45 +0100

    create own action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,43 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as exec from '@actions/exec';
+import * as os from 'os'
+import * as uuid from "uuid/v1";
 
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+const url: string = core.getInput('url');
+const branch: string = core.getInput('branch');
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+const homeDirectory = os.homedir();
+const workingDirectory = `${homeDirectory}/action-install-swift-tool-${uuid()}`;
+const productDirectory = `${workingDirectory}/.build/release`;
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+async function create_working_directory(): Promise<void> {
+  await group('Create working directory...', async () => {
+    await exec.exec('mkdir', ['-p', workingDirectory]);
+  })
 }
 
-run();
+async function clone_git(): Promise<void> {
+  await group('Clone repo...', async () => {
+    if (branch) {
+      await exec.exec('git', ['clone', '--depth', '1', '--branch', branch, url, workingDirectory]);
+    } else {
+      await exec.exec('git', ['clone', '--depth', '1', url, workingDirectory]);
+    }
+  })
+}
+
+async function build_tool(): Promise<void> {
+  await group('Build tool...', async () => {
+    await exec.exec('cd', [workingDirectory])
+    await exec.exec('swift', ['build', '--configuration', 'release', '--disable-sandbox']);
+  })
+}
+
+async function main(): Promise<void> {
+  await create_working_directory();
+  await clone_git();
+  await build_tool();
+  core.addPath(productDirectory);
+}
+
+main().catch(error => { core.setFailed(error.message); })
