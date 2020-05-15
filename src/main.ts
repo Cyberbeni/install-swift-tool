9f240f5f925cc4e4922562c9f251c1141fd372c3
Benedek Kozma <cyberbeni@gmail.com>
2020-05-15 23:42:24 +0200

    relative working dir

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import * as cache from '@actions/cache';
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as os from 'os'
-import uuidv5 from 'uuid/v5';
-import { isUndefined } from 'util';
+import uuidv5 from 'uuid/v5'
+import { isUndefined } from 'util'
 
-const url: string = core.getInput('url');
-const branch: string = core.getInput('branch');
-const useCache: boolean = core.getInput('use-cache') == 'true';
+const url: string = core.getInput('url')
+const branch: string = core.getInput('branch')
+const useCache: boolean = core.getInput('use-cache') == 'true'
 
-const homeDirectory = os.homedir();
+const homeDirectory = os.homedir()
 
 /** vvv HELPERS vvv */
 
@@ -25,33 +25,35 @@ async function better_exec(commandLine: string, args?: string[]): Promise<string
 
 /** ^^^ HELPERS ^^^ */
 
-let uuid: string = '';
-let swiftVersion = '';
-let workingDirectory = '';
-let productDirectory = '';
-let commitHash: string = '';
+let uuid: string = ''
+let swiftVersion = ''
+let workingDirectory = ''
+let productDirectory = ''
+let commitHash: string = ''
 async function create_working_directory(): Promise<void> {
   await core.group('Create working directory...', async () => {
     if (branch) {
-      commitHash = await better_exec('git', ['ls-remote', '-ht', url, `refs/heads/${branch}`, `refs/tags/${branch}`]);
+      commitHash = await better_exec('git', ['ls-remote', '-ht', url, `refs/heads/${branch}`, `refs/tags/${branch}`])
     } else {
-      commitHash = await better_exec('git', ['ls-remote', url, `HEAD`]);
+      commitHash = await better_exec('git', ['ls-remote', url, `HEAD`])
     }
-    commitHash = commitHash.substring(0,39);
-    swiftVersion = await better_exec('swift', ['-version']);
+    commitHash = commitHash.substring(0,39)
+    swiftVersion = await better_exec('swift', ['-version'])
 
-    uuid = uuidv5(`${url}-${commitHash}-${swiftVersion}`, '6050636b-7499-41d4-b9c6-756aff9856d0');
-    workingDirectory = `${homeDirectory}/install-swift-tool-${uuid}`;
-    productDirectory = `${workingDirectory}/.build/release`;
+    uuid = uuidv5(`${url}-${commitHash}-${swiftVersion}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
+    workingDirectory = `install-swift-tool-${uuid}`
+    productDirectory = `${workingDirectory}/.build/release`
 
-    await exec.exec('mkdir', ['-p', productDirectory]);
+    await exec.exec('mkdir', ['-p', workingDirectory])
   })
 }
 
-let didRestore: boolean = false;
+let cacheKey: string = ''
+let didRestore: boolean = false
 async function try_to_restore(): Promise<void> {
   await core.group('Try to restore from cache...', async () => {
-    didRestore = !isUndefined(await cache.restoreCache([productDirectory], `installswifttool-${uuid}`))
+    cacheKey = `installswifttool-${uuid}`
+    didRestore = !isUndefined(await cache.restoreCache([productDirectory], cacheKey))
     core.setOutput('cache-hit', `${didRestore}`)
   })
 }
@@ -59,45 +61,45 @@ async function try_to_restore(): Promise<void> {
 async function clone_git(): Promise<void> {
   await core.group('Clone repo...', async () => {
     if (branch) {
-      await exec.exec('git', ['clone', '--depth', '1', '--branch', branch, url, workingDirectory]);
+      await exec.exec('git', ['clone', '--depth', '1', '--branch', branch, url, workingDirectory])
     } else {
-      await exec.exec('git', ['clone', '--depth', '1', url, workingDirectory]);
+      await exec.exec('git', ['clone', '--depth', '1', url, workingDirectory])
     }
-    await exec.exec('git', ['-C', workingDirectory,'rev-parse', 'HEAD']);
+    await exec.exec('git', ['-C', workingDirectory,'rev-parse', 'HEAD'])
   })
 }
 
 async function build_tool(): Promise<void> {
   await core.group('Build tool...', async () => {
-    await exec.exec('swift', ['build', '--package-path', workingDirectory, '--configuration', 'release', '--disable-sandbox']);
+    await exec.exec('swift', ['build', '--package-path', workingDirectory, '--configuration', 'release', '--disable-sandbox'])
   })
 }
 
 async function save_to_cache(): Promise<void> {
   await core.group('Save to cache...', async () => {
-    await cache.saveCache([productDirectory], `installswifttool-${uuid}`)
+    await cache.saveCache([productDirectory], cacheKey)
   })
 }
 
 async function export_path(): Promise<void> {
   await core.group('Export path...', async () => {
-    core.addPath(productDirectory);
+    core.addPath(productDirectory)
   })
 }
 
 async function main(): Promise<void> {
-  await create_working_directory();
+  await create_working_directory()
   if (useCache) {
     await try_to_restore()
   }
   if (!didRestore) {
-    await clone_git();
-    await build_tool();
+    await clone_git()
+    await build_tool()
     if (useCache) {
       await save_to_cache()
     }
   }
-  await export_path();
+  await export_path()
 }
 
 main().catch(error => { core.setFailed(error.message); })
