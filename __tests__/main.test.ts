1c9ad4651440f44ddfda67314ed7b52e4ec405c1
Benedek Kozma <cyberbeni@gmail.com>
2020-10-03 00:51:03 +0200

    indentation fix

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,11 +2,11 @@ import * as semver from 'semver'
 import { better_exec } from '../src/helpers'
 
 test('Get versions', async() => {
-    let versionsString = await better_exec('git', ['ls-remote', '--refs', '--tags', 'https://github.com/realm/SwiftLint'])
-    let versions = versionsString.split('\n').map(function(value) {
-        return value.split('/').pop() ?? ''
-    })
-    let targetVersion = semver.maxSatisfying(versions, '^0')
-    console.log(versions)
-    console.log(targetVersion)
+  let versionsString = await better_exec('git', ['ls-remote', '--refs', '--tags', 'https://github.com/realm/SwiftLint'])
+  let versions = versionsString.split('\n').map(function(value) {
+    return value.split('/').pop() ?? ''
+  })
+  let targetVersion = semver.maxSatisfying(versions, '^0')
+  console.log(versions)
+  console.log(targetVersion)
 });
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,11 @@
 import * as exec from '@actions/exec'
 
 export async function better_exec(commandLine: string, args?: string[]): Promise<string> {
-    let output: string = ''
-    await exec.exec(commandLine, args, {
-        listeners: {
-            stdout: (data: Buffer) => { output += data.toString().trim() }
-        }
-    })
-    return output
+  let output: string = ''
+  await exec.exec(commandLine, args, {
+    listeners: {
+      stdout: (data: Buffer) => { output += data.toString().trim() }
+    }
+  })
+  return output
 }
