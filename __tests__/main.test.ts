afad4ed837620a9dec1cfaea45ddd2bda1501aa0
Benedek Kozma <cyberbeni@gmail.com>
2020-10-02 22:47:54 +0200

    fix buffer usage

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,18 +5,18 @@ async function better_exec(commandLine: string, args?: string[]): Promise<string
     let output: string = ''
     await exec.exec(commandLine, args, {
       listeners: {
-        stdout: (data: Buffer) => { output = data.toString().trim() }
+        stdout: (data: Buffer) => { output += data.toString().trim() }
       }
     })
     return output
   }
 
 test('Get versions', async() => {
-    let versionsString = await better_exec('git', ['ls-remote', '--tags', 'https://github.com/Cyberbeni/install-swift-tool.git'])
+    let versionsString = await better_exec('git', ['ls-remote', '--tags', 'https://github.com/realm/SwiftLint'])
     let versions = versionsString.split('\n').map(function(value, index, array) {
         return value.split('/').pop() ?? ''
     })
-    let targetVersion = semver.maxSatisfying(versions, '^3.0')
+    let targetVersion = semver.maxSatisfying(versions, '^0')
     console.log(versions)
     console.log(targetVersion)
 });
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function better_exec(commandLine: string, args?: string[]): Promise<string
   let output: string = ''
   await exec.exec(commandLine, args, {
     listeners: {
-      stdout: (data: Buffer) => { output = data.toString().trim() }
+      stdout: (data: Buffer) => { output += data.toString().trim() }
     }
   })
   return output
