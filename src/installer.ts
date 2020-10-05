06ee8ffb792675c4ab2c3287a2425f139653be9b
Benedek Kozma <cyberbeni@gmail.com>
2020-10-05 23:36:24 +0200

    separate github action input and installer flow

---
+++ src/installer.ts
@@ -0,0 +1,140 @@
+import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as os from 'os'
+import * as semver from 'semver'
+
+import { exec, getUuid } from './helpers'
+
+export class SwiftToolInstaller {
+  
+  // Input
+
+  readonly url: string
+  branch: string
+  readonly version: string
+  readonly useCache: boolean
+
+  private constructor(url: string, branch: string, version: string, useCache: boolean) {
+    this.url = url
+    this.branch = branch
+    this.version = version
+    this.useCache = useCache
+  }
+
+  // Steps
+
+  async resolve_version(): Promise<void> {
+    await core.group('Resolving version requirement', async () => {
+      let versions = (await exec('git', ['ls-remote', '--refs', '--tags', this.url]))
+        .split('\n')
+        .map(function(value) {
+          return value.split('/').pop() ?? ''
+        })
+      let targetVersion = semver.maxSatisfying(versions, this.version)
+      if (targetVersion) {
+        core.info(`Resolved version: ${targetVersion}`)
+        this.branch = targetVersion
+      } else {
+        throw Error(`No version satisfying '${this.version}' found.`)
+      }
+    })
+  }
+
+  uuid: string = ''
+  cacheKey: string = ''
+  workingDirectory = ''
+  productDirectory = ''
+  cacheDirectory = ''
+  updateDirectoryNames(newUuid: string) {
+    this.uuid = newUuid
+    this.cacheKey = `installswifttool-${this.uuid}`
+    this.workingDirectory = `${os.homedir()}/install-swift-tool-${this.uuid}`
+    this.productDirectory = `${this.workingDirectory}/.build/release`
+    this.cacheDirectory = `${this.workingDirectory}/.build/*/release`
+  }
+  async create_working_directory(): Promise<void> {
+    await core.group('Creating working directory', async () => {
+      let commitHash: string = ''
+      if (this.branch) {
+        commitHash = await exec('git', ['ls-remote', '-ht', this.url, `refs/heads/${this.branch}`, `refs/tags/${this.branch}`])
+      } else {
+        commitHash = await exec('git', ['ls-remote', this.url, `HEAD`])
+      }
+      commitHash = commitHash.substring(0,40)
+      this.updateDirectoryNames(await getUuid(this.url, commitHash))
+      await exec('mkdir', ['-p', this.workingDirectory])
+    })
+  }
+
+  didRestore: boolean = false
+  async try_to_restore(): Promise<void> {
+    await core.group('Trying to restore from cache', async () => {
+      this.didRestore = await cache.restoreCache([this.cacheDirectory, this.productDirectory], this.cacheKey) !== undefined
+      core.setOutput('cache-hit', `${this.didRestore}`)
+    })
+  }
+
+  async clone_git(): Promise<void> {
+    await core.group('Cloning repo', async () => {
+      if (this.branch) {
+        await exec('git', ['clone', '--depth', '1', '--branch', this.branch, this.url, this.workingDirectory])
+      } else {
+        await exec('git', ['clone', '--depth', '1', this.url, this.workingDirectory])
+      }
+      const commitHash = await exec('git', ['-C', this.workingDirectory,'rev-parse', 'HEAD'])
+      const newUuid = await getUuid(this.url, commitHash)
+      if (this.uuid != newUuid) {
+        const oldWorkingDirectory = this.workingDirectory
+        this.updateDirectoryNames(newUuid)
+        await exec('mv', [oldWorkingDirectory, this.workingDirectory])
+      }
+    })
+  }
+
+  async build_tool(): Promise<void> {
+    await core.group('Building tool', async () => {
+      await exec('swift', ['build', '--package-path', this.workingDirectory, '--configuration', 'release', '--disable-sandbox'])
+    })
+  }
+
+  async try_to_cache(): Promise<void> {
+    await core.group('Trying to save to cache', async () => {
+      try {
+        await cache.saveCache([this.cacheDirectory, this.productDirectory], this.cacheKey)
+      } catch (error) {
+        core.info(error.message)
+      }
+    })
+  }
+
+  async export_path(): Promise<void> {
+    await core.group('Exporting path', async () => {
+      core.addPath(this.productDirectory)
+    })
+  }
+
+  // Run
+
+  async install(): Promise<void> {
+    if (this.version) {
+      await this.resolve_version()
+    }
+    await this.create_working_directory()
+    if (this.useCache) {
+      await this.try_to_restore()
+    }
+    if (!this.didRestore) {
+      await this.clone_git()
+      await this.build_tool()
+      if (this.useCache) {
+        await this.try_to_cache()
+      }
+    }
+    await this.export_path()
+  }
+
+  static async install(url: string, branch: string, version: string, useCache: boolean): Promise<void> {
+    const installer = new SwiftToolInstaller(url, branch, version, useCache)
+    await installer.install()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,125 +1,17 @@
-import * as cache from '@actions/cache'
 import * as core from '@actions/core'
-import * as os from 'os'
-import * as semver from 'semver'
-
-import { exec, getUuid } from './helpers'
+import { SwiftToolInstaller } from './installer'
 
 // Inputs
 
 const url: string = core.getInput('url')
-let branch: string = core.getInput('branch')
+const branch: string = core.getInput('branch')
 const version: string = core.getInput('version')
 const useCache: boolean = core.getInput('use-cache') == 'true'
 
-// Steps
-
-async function resolve_version(): Promise<void> {
-  await core.group('Resolving version requirement', async () => {
-    let versions = (await exec('git', ['ls-remote', '--refs', '--tags', url]))
-      .split('\n')
-      .map(function(value) {
-        return value.split('/').pop() ?? ''
-      })
-    let targetVersion = semver.maxSatisfying(versions, version)
-    if (targetVersion) {
-      core.info(`Resolved version: ${targetVersion}`)
-      branch = targetVersion
-    } else {
-      throw Error(`No version satisfying '${version}' found.`)
-    }
-  })
-}
-
-let uuid: string = ''
-let workingDirectory = ''
-let productDirectory = ''
-let cacheDirectory = ''
-function updateDirectoryNames(newUuid: string) {
-  uuid = newUuid
-  workingDirectory = `${os.homedir()}/install-swift-tool-${uuid}`
-  productDirectory = `${workingDirectory}/.build/release`
-  cacheDirectory = `${workingDirectory}/.build/*/release`
-}
-async function create_working_directory(): Promise<void> {
-  await core.group('Creating working directory', async () => {
-    let commitHash: string = ''
-    if (branch) {
-      commitHash = await exec('git', ['ls-remote', '-ht', url, `refs/heads/${branch}`, `refs/tags/${branch}`])
-    } else {
-      commitHash = await exec('git', ['ls-remote', url, `HEAD`])
-    }
-    commitHash = commitHash.substring(0,40)
-    updateDirectoryNames(await getUuid(url, commitHash))
-    await exec('mkdir', ['-p', workingDirectory])
-  })
-}
-
-let cacheKey: string = ''
-let didRestore: boolean = false
-async function try_to_restore(): Promise<void> {
-  await core.group('Trying to restore from cache', async () => {
-    cacheKey = `installswifttool-${uuid}`
-    didRestore = await cache.restoreCache([cacheDirectory, productDirectory], cacheKey) !== undefined
-    core.setOutput('cache-hit', `${didRestore}`)
-  })
-}
-
-async function clone_git(): Promise<void> {
-  await core.group('Cloning repo', async () => {
-    if (branch) {
-      await exec('git', ['clone', '--depth', '1', '--branch', branch, url, workingDirectory])
-    } else {
-      await exec('git', ['clone', '--depth', '1', url, workingDirectory])
-    }
-    const commitHash = await exec('git', ['-C', workingDirectory,'rev-parse', 'HEAD'])
-    const newUuid = await getUuid(url, commitHash)
-    if (uuid != newUuid) {
-      const oldWorkingDirectory = workingDirectory
-      updateDirectoryNames(newUuid)
-      await exec('mv', [oldWorkingDirectory, workingDirectory])
-    }
-  })
-}
-
-async function build_tool(): Promise<void> {
-  await core.group('Building tool', async () => {
-    await exec('swift', ['build', '--package-path', workingDirectory, '--configuration', 'release', '--disable-sandbox'])
-  })
-}
-
-async function try_to_cache(): Promise<void> {
-  await core.group('Trying to save to cache', async () => {
-    try {
-      await cache.saveCache([cacheDirectory, productDirectory], cacheKey)
-    } catch (error) {
-      core.info(error.message)
-    }
-  })
-}
-
-async function export_path(): Promise<void> {
-  await core.group('Exporting path', async () => {
-    core.addPath(productDirectory)
-  })
-}
+// Run
 
 async function main(): Promise<void> {
-  if (version) {
-    await resolve_version()
-  }
-  await create_working_directory()
-  if (useCache) {
-    await try_to_restore()
-  }
-  if (!didRestore) {
-    await clone_git()
-    await build_tool()
-    if (useCache) {
-      await try_to_cache()
-    }
-  }
-  await export_path()
+  SwiftToolInstaller.install(url, branch, version, useCache)
 }
 
 main().catch(error => { core.setFailed(error.message); })
