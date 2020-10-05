653c79a78511062e45cc5d14b2192e403feeece8
Benedek Kozma <cyberbeni@gmail.com>
2020-10-05 17:17:53 +0200

    recheck commit hash after cloning

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,6 @@
 import { exec as _exec } from '@actions/exec'
+import * as os from 'os'
+import { v5 as _uuid } from 'uuid'
 
 export async function exec(commandLine: string, args?: string[]): Promise<string> {
   let output: string = ''
@@ -8,4 +10,9 @@ export async function exec(commandLine: string, args?: string[]): Promise<string
     }
   })
   return output
+}
+
+export async function getUuid(url: string, commitHash: string): Promise<string> {
+  const swiftVersion = await exec('swift', ['-version'])
+  return _uuid(`${url}-${commitHash}-${os.version}-${swiftVersion}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
 }
