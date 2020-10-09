12d82137a1af16cc5d6c4436786e092c34ad1c67
Benedek Kozma <cyberbeni@gmail.com>
2020-10-09 21:01:09 +0200

    only use architecture for uuid when on macOS above 10.14.4

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,6 @@
 import { exec as _exec } from '@actions/exec'
 import * as os from 'os'
+import * as semver from 'semver'
 import { v5 as _uuid } from 'uuid'
 
 export async function exec(commandLine: string, args?: string[]): Promise<string> {
@@ -14,5 +15,12 @@ export async function exec(commandLine: string, args?: string[]): Promise<string
 
 export async function getUuid(url: string, commitHash: string): Promise<string> {
   const swiftVersion = await exec('swift', ['-version'])
-  return _uuid(`${url}-${commitHash}-${os.version}-${swiftVersion}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
+  let additionalInfo = `${os.version()}-${swiftVersion}`
+  if (os.platform() == "darwin") {
+    let macVersion = await exec('sw_vers', ['-productVersion'])
+    if (semver.gte(macVersion, "10.14.4")) {
+      additionalInfo = `macos-${os.arch()}`
+    }
+  }
+  return _uuid(`${url}-${commitHash}-${additionalInfo}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
 }
---
+++ types/helpers.d.ts
@@ -0,0 +1,2 @@
+export declare function exec(commandLine: string, args?: string[]): Promise<string>;
+export declare function getUuid(url: string, commitHash: string): Promise<string>;
