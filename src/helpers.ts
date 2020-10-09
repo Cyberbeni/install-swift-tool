01c4d9efa4276c667cd148ee348d96ecc9340f42
Benedek Kozma <cyberbeni@gmail.com>
2020-10-09 22:38:12 +0200

    cleanup

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core'
 import { exec as _exec } from '@actions/exec'
 import * as os from 'os'
-import * as semver from 'semver'
 import { v5 as _uuid } from 'uuid'
 
 export async function exec(commandLine: string, args?: string[]): Promise<string> {
@@ -16,29 +15,18 @@ export async function exec(commandLine: string, args?: string[]): Promise<string
 
 export async function getUuid(url: string, commitHash: string): Promise<string> {
   let additionalInfo: string
-  switch (os.platform()) {
-    case "darwin": {
-      let macVersion = await exec('sw_vers', ['-productVersion'])
-      if (semver.gte(macVersion, "10.14.4")) {
-        additionalInfo = `macos-${os.arch()}`
-      } else {
-        additionalInfo = `macos-embed-swift-${os.arch()}`
-      }
-      break
+  if (os.platform() == "darwin") {
+    additionalInfo = `macos-${os.arch()}`
+  } else {
+    let osVersion: string
+    if (os.version != undefined) {
+      osVersion = os.version()
+    } else {
+      core.warning('os.version undefined, using `uname -v` instead')
+      osVersion = await exec('uname', ['-v']) // os.version is somehow undefined on GitHub runner
     }
-    case "linux": {
-      const osCodename = await exec('lsb_release', ['-c'])
-      const kernelVersion = os.release()
-      const swiftVersion = await exec('swift', ['-version'])
-      additionalInfo = `${osCodename}-${kernelVersion}-${swiftVersion}`
-      break
-    }
-    default: {
-      const osVersion = await exec('uname', ['-v']) // os.version is somehow undefined on GitHub runner
-      const swiftVersion = await exec('swift', ['-version'])
-      additionalInfo = `${osVersion}-${swiftVersion}`
-    }
+    const swiftVersion = await exec('swift', ['-version'])
+    additionalInfo = `${osVersion}-${swiftVersion}`
   }
-  core.info(additionalInfo)
   return _uuid(`${url}-${commitHash}-${additionalInfo}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
 }
