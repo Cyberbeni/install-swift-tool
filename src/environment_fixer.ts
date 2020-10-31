e60fd5cd9b29c2c9ab03e31811c033598d8759ff
Benedek Kozma <cyberbeni@gmail.com>
2020-10-31 12:59:43 +0100

    check multiple possible paths for sourcekit

--- src/environment_fixer.ts
+++ src/environment_fixer.ts
@@ -1,15 +1,32 @@
 import * as core from '@actions/core'
+import * as fs from 'fs'
 import * as os from 'os'
 import { env } from 'process'
 
 export class SwiftEnvironmentFixer {
   static async fixSourceKitPath () {
-    const envVar='LINUX_SOURCEKIT_LIB_PATH'
+    const envVar = 'LINUX_SOURCEKIT_LIB_PATH'
     if(env[envVar] != undefined) {
       return
     }
     await core.group(`Setting ${envVar}`, async () => {
-      core.exportVariable(envVar, '/usr/share/swift/usr/lib')
+      let exported = false
+      const libName = 'libsourcekitdInProc.so'
+      const possiblePaths = ['/usr/share/swift/usr/lib', '/usr/lib']
+      for (const path of possiblePaths) {
+        fs.stat(`${path}/${libName}`, (err) => {
+          if (err == null) {
+            core.exportVariable(envVar, path)
+            exported = true
+          }
+        })
+        if (exported) {
+          break
+        }
+      }
+      if (!exported) {
+        core.warning(`Failed to find suitable path for ${envVar}`)
+      }
     })
   }
 
