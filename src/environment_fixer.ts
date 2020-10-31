808f3d0d273fed5b5734a606e2d72e756967e1b8
Benedek Kozma <cyberbeni@gmail.com>
2020-10-31 12:46:36 +0100

    restore fix path export, change version for swiftlint in test

--- src/environment_fixer.ts
+++ src/environment_fixer.ts
@@ -2,8 +2,6 @@ import * as core from '@actions/core'
 import * as os from 'os'
 import { env } from 'process'
 
-import { exec } from './helpers'
-
 export class SwiftEnvironmentFixer {
   static async fixSourceKitPath () {
     const envVar='LINUX_SOURCEKIT_LIB_PATH'
@@ -11,8 +9,7 @@ export class SwiftEnvironmentFixer {
       return
     }
     await core.group(`Setting ${envVar}`, async () => {
-      const swiftPath = await exec('which', ['swift'])
-      core.exportVariable(envVar, `${swiftPath}/../lib`)
+      core.exportVariable(envVar, '/usr/share/swift/usr/lib')
     })
   }
 
