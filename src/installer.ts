0c0add8d674fa20bd5b2a12d4c46a17869be492b
Benedek Kozma <cyberbeni@gmail.com>
2022-09-27 19:47:05 +0200

    make commit readonly

--- src/installer.ts
+++ src/installer.ts
@@ -10,7 +10,7 @@ export class SwiftToolInstaller {
 	// Input
 
 	readonly url: string
-	commit: string
+	readonly commit: string
 	branch: string
 	readonly version: string
 	readonly useCache: boolean
