677728925e09dfb8caeac36662785856613b2758
Benedek Kozma <cyberbeni@gmail.com>
2024-10-13 23:11:55 +0200

    Turn local test into actual test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,8 @@
-test('Local test', async() => {
-  const test = "test"
-  expect(test.length).toBe(4)
+import { PackageResolved } from '../src/vendor/SwiftPackage'
+import fs from 'fs'
+
+test('Test Package.resolved v3 parsing', async() => {
+  const packageResolvedText = fs.readFileSync("./Package.resolved", { encoding: 'utf8' })
+  const packageResolved = new PackageResolved(packageResolvedText)
+  expect(packageResolved.pins[0].location).toBe("https://github.com/nicklockwood/SwiftFormat")
 })
