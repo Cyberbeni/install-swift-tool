ab76bc85707fe07d6a133605f79e47fbe03983e6
Benedek Kozma <cyberbeni@gmail.com>
2024-10-13 23:17:56 +0200

    expect commit hash in test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,5 +3,5 @@ import { SwiftToolInstaller } from '../src/installer'
 test('Test Package.resolved v3 parsing', async() => {
   const installer = new SwiftToolInstaller("https://github.com/nicklockwood/SwiftFormat", "", "" , "", false)
   const version = installer.parsePackageResolved()
-  expect(version).toBe("0.54.5")
+  expect(version).toBe("ab6844edb79a7b88dc6320e6cee0a0db7674dac3")
 })
