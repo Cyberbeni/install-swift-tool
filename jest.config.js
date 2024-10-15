9d5a1db11cdb242ee468feff1bfc6e1d1f184d97
Benedek Kozma <cyberbeni@gmail.com>
2024-10-15 09:31:14 +0200

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
-  testMatch: ['__tests__/**/*.test.ts'],
+  testMatch: ['**/__tests__/*.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
