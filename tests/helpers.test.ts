6a089c00414bf33747597f65bc5770f8aa680a78
Benedek Kozma <cyberbeni@gmail.com>
2024-10-16 00:21:54 +0200

    add another exec test

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,8 +1,11 @@
 import * as h from '../src/helpers'
 
 describe('exec', () => {
+	test('throw error when the command doesn\'t exist', async () => {
+		await expect(h.exec('this-command-doesnt-exist', [])).rejects.toThrow()
+	})
 	test('throw error when non-zero exit code', async () => {
-		await expect(h.exec('this-command-doesnt-exits', [])).rejects.toThrow()
+		await expect(h.exec('git', ['this-subcommand-doesnt-exist'])).rejects.toThrow()
 	})
 	test('return output when zero exit code', async () => {
 		expect(await h.exec('git', ['--version'])).toMatch(/^git version /)
