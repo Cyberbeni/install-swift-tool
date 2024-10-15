a7acd3634189354749e5affffdd77df6aac6c923
Benedek Kozma <cyberbeni@gmail.com>
2024-10-15 09:08:48 +0200

    Use extends in new eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,31 +2,28 @@ import globals from "globals"
 import js from "@eslint/js"
 import ts from "typescript-eslint"
 
-export default [
-	js.configs.recommended,
-	...ts.configs.recommended,
-	{
-		files: ["src/**/*.ts"],
-		ignores: ["**/vendor/**/*.ts"],
-		languageOptions: {
-			parser: ts.parser,
-			parserOptions: {
-				sourceType: "script",
-				project: "tsconfig.json"
-			},
-			globals: {
-				...globals.node
-			}
+export default ts.config({
+	files: ["src/**/*.ts"],
+	ignores: ["src/vendor/**"],
+	extends: [
+		js.configs.recommended,
+		...ts.configs.recommended,
+	],
+	languageOptions: {
+		parser: ts.parser,
+		parserOptions: {
+			sourceType: "script",
+			project: "tsconfig.json"
 		},
-		plugins: {
-			ts: ts.plugin
-		},
-		rules: {
-			"max-len": ["warn", { "code": 140 }],
-			"indent": ["warn", "tab"],
-			"quotes": ["warn", "single"],
-			"semi": ["warn", "never"],
-			"ts/no-floating-promises": ["error", { "ignoreVoid": true }]
+		globals: {
+			...globals.node
 		}
+	},
+	rules: {
+		"max-len": ["warn", { "code": 140 }],
+		"indent": ["warn", "tab"],
+		"quotes": ["warn", "single"],
+		"semi": ["warn", "never"],
+		"@typescript-eslint/no-floating-promises": ["error", { "ignoreVoid": true }]
 	}
-]
+})
