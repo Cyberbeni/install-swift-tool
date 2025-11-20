ec0275fded83483a67a050a280b21e51da07a8d7
Benedek Kozma <cyberbeni@gmail.com>
2025-11-20 20:36:41 +0100

    Try removing uuid dependency

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core'
 import { getExecOutput } from '@actions/exec'
 import * as os from 'os'
-import { v5 as _uuid } from 'uuid'
 
 export async function exec(commandLine: string, args: string[]): Promise<string> {
 	const { stdout } = await getExecOutput(commandLine, args)
@@ -17,7 +16,7 @@ export async function getUuid(url: string, commitHash: string): Promise<string> 
 		const swiftVersion = await exec('swift', ['-version'])
 		additionalInfo = `${osVersion}-${os.arch()}-${swiftVersion}`
 	}
-	return _uuid(`${url}-${commitHash}-${additionalInfo}`, '6050636b-7499-41d4-b9c6-756aff9856d0')
+	return `${url}-${commitHash}-${additionalInfo}`
 }
 
 export function errorMessage(error: unknown): string {
