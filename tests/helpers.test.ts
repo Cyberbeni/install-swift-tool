2b53861ab12618376ca3ddc835abf24905b9c223
Benedek Kozma <cyberbeni@gmail.com>
2026-02-01 13:59:50 +0100

    Update eslint rules

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,5 +1,5 @@
 import { describe, test } from 'node:test'
-import assert from 'node:assert/strict';
+import assert from 'node:assert/strict'
 
 import * as h from '../src/helpers.js'
 
--- tests/installer.test.ts
+++ tests/installer.test.ts
@@ -1,5 +1,5 @@
 import { describe, test } from 'node:test'
-import assert from 'node:assert/strict';
+import assert from 'node:assert/strict'
 
 import { SwiftToolInstaller } from '../src/installer.js'
 
